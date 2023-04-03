78db829edfc08e11953c38e46cbb68ed3c8322d3
rkvines <128716512+rkvines@users.noreply.github.com>
2023-04-03 19:57:46 +0530

    added main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import turtle as t
+# fill color in turtule ry zon application
+t.fillcolor("yellow")
+t.begin_fill()
+for i in range(9):
+    t.fd(200)
+    t.rt(160)
+
+#fill color tag in ritesh owner of ryzone gaming entertainment
+t.end_fill()
